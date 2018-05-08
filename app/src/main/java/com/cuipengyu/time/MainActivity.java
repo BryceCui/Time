eeff6091ca08b261e006ec6f199ac97db6c20f01
cuipengyu <cuipengyusoul@gmail.com>
2018-05-08 14:45:48 +0800

    Initial commit

---
+++ app/src/main/java/com/cuipengyu/time/MainActivity.java
@@ -0,0 +1,71 @@
+package com.cuipengyu.time;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+
+    private TimeChangeReceiver timeChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_TIME_TICK);//每分钟变化
+        intentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);//设置了系统时区
+        intentFilter.addAction(Intent.ACTION_TIME_CHANGED);//设置了系统时间
+        timeChangeReceiver = new TimeChangeReceiver();
+        registerReceiver(timeChangeReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(timeChangeReceiver);
+    }
+
+    class TimeChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+      
+            //Objects.requireNonNull 空安全
+            switch (Objects.requireNonNull(intent.getAction())) {
+                case Intent.ACTION_TIME_TICK:
+                    Date day = new Date();
+                    SimpleDateFormat simp = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.MINUTE, 5);
+                    String time = simp.format(day);
+                    String submittime = simp.format(calendar.getTime());
+                    Log.e("time", time);
+                    Log.e("submittime", submittime);
+                    break;
+                case Intent.ACTION_TIME_CHANGED:
+                    //设置了系统时间
+                    Toast.makeText(context, "system time changed", Toast.LENGTH_SHORT).show();
+                    break;
+                case Intent.ACTION_TIMEZONE_CHANGED:
+                    //设置了系统时区的action
+                    Toast.makeText(context, "system time zone changed", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+
+}
